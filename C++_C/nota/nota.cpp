265990c083d960f2ab34ba0628988d67875bb831
MCosta-03 <gilcostabn@gmail.com>
2025-10-01 12:06:42 +0100

    Add files via upload

---
+++ C++_C/nota/nota.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+	float nota = 0;
+
+	std::cout << "Diz a tua nota: ";
+	std::cin >> nota;
+
+	if (nota < 20 && nota > 0) {
+		if (nota > 9.5) {
+			std::cout << "Estás aprovado!";
+		}
+		else {
+			std::cout << "Não foste aprovado!";
+		}
+	}
+}
