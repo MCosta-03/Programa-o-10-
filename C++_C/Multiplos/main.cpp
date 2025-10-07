eb1147abfb6f29c637dfef215da22682c4deee74
MCosta-03 <gilcostabn@gmail.com>
2025-10-07 14:11:13 +0100

    Add files via upload

---
+++ C++_C/Multiplos/main.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <locale>
+
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Portuguese");
+
+	int num = 0;
+
+	do {
+		cout << "Diz um número: ";
+		cin >> num;
+
+		if (num == 0) {
+			return 0;
+		}
+
+		if ((num % 5) == 0) {
+			cout << "É multiplo 5!\n";
+		}
+		else {
+			cout << "Não é multiplo 5!\n";
+		}
+
+		if ((num % 3) == 0) {
+			cout << "É multiplo 3!\n";
+		}
+		else {
+			cout << "Não é multiplo 3!\n";
+		}
+
+	} while (num != 0);
+
+	return 0;
+}
