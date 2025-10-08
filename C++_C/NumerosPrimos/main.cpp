bb67aace6e14f5492da0c895cf4522f5f02db5d0
MCosta-03 <gilcostabn@gmail.com>
2025-10-08 12:29:28 +0100

    Add files via upload

---
+++ C++_C/NumerosPrimos/main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <locale>
+
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Portuguese");
+	
+	int num = 0;
+	cout << "Diz um número: ";
+	cin >> num;
+
+	int times = 0;
+
+	for (int i = 1; i <= num; i++) {
+		if ((num % i) == 0) {
+			times++;
+		}
+	}
+
+	if (times == 2 || num == 1) {
+		cout << "O número é primo!";
+	}
+	else {
+		cout << "O número não é primo!";
+	}	
+
+	return 0;
+}
