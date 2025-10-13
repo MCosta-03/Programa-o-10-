1e26ea5c5d16750605328cde8005b1b91838fd20
MCosta-03 <gilcostabn@gmail.com>
2025-10-13 12:22:18 +0100

    Add files via upload

---
+++ C++_C/EscolherDisciplinas/main.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <locale>
+#include <string>
+
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Portuguese");
+
+	char c = 'n';
+	string Escolhas[4] = { "Matemática", "Português", "Inglês", "Fisica Química"};
+	int escolha;
+
+	cout << "Vou fazer uma pergunta e tu vais escolher...\n\n";
+
+	while (c == 'n') {
+		cout << "\nEscolhe um número de 0 a 3: ";
+		cin >> escolha;
+
+		if (escolha < 4 && escolha > -1) {
+			cout << "Escolheste o " << Escolhas[escolha] << " !\n";
+		}
+		else {
+			cout << "\nEscolha inválida!\n";
+		}
+
+		cout << "Queres sair? (y/n): ";
+		cin >> c;
+	}
+	
+	return 0;
+}
