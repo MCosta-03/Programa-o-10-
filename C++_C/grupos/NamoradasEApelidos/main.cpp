e91d39f8cfd0f2c680e29893eaeec1f870bcf9b9
MCosta-03 <gilcostabn@gmail.com>
2025-10-15 12:19:13 +0100

    Add files via upload

---
+++ C++_C/grupos/NamoradasEApelidos/main.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <locale>
+#include <string>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Portuguese");
+
+	string Nomes[5];
+	string SubNom[5];
+	char c = 'n';
+	int Select = 0;
+
+	for (int i = 0; i < 5; i++) {
+		cout << "Diz o nome da tua " << i+1 << "º namorada!: ";
+		cin >> Nomes[i];
+	}
+
+	for (int i = 0; i < 5; i++) {
+		cout << "Diz o subnome da tua " << i+1 << "º namorada!: ";
+		cin >> SubNom[i];
+	}
+
+	while (c == 'n') {
+		cout << "Diz aí um número de 1 a 5: ";
+		cin >> Select;
+
+		cout << "o nome é : " << Nomes[Select] << " e o apelido é " << SubNom[Select] << " !";
+
+		cout << "Queres continuar? :";
+		cin >> c;
+	}
+
+	return 0;
+}
