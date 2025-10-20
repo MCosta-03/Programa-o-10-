037d03222e1bbbc44dd0035e4a30037da35a4479
MCosta-03 <gilcostabn@gmail.com>
2025-10-20 12:51:23 +0100

    Add files via upload

---
+++ C++_C/Adivinha/main.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <locale>
+#include <string>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Portuguese");
+	srand(time(0));
+
+	int num = rand() % 100 + 1;
+	int usr_num = 0;
+
+	cout << "Vou escolher um número de 0 a 100...\n";
+
+	while (usr_num != num) {
+		cout << "Tenta adivinhar o número: ";
+		cin >> usr_num;
+
+		if (usr_num == num) {
+
+		}
+		else if(usr_num < num){
+			cout << "Pista: mais a cima...\n";
+		}
+		else if (usr_num > num) {
+			cout << "Pista: mais a baixo...\n";
+		}
+	}
+
+	cout << "Acertaste!!!!";
+	
+	return 0;
+}
