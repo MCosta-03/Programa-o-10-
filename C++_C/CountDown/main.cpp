f57c5860f03cf7a6b551d0d6d14ead64b32c3b6d
MCosta-03 <gilcostabn@gmail.com>
2025-10-22 12:25:28 +0100

    Add files via upload

---
+++ C++_C/CountDown/main.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <locale>
+#include <string>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Portuguese");
+	
+	int num = 0;
+	cout << "Diz um número: ";
+	cin >> num;
+
+	for (int i = num; i > -1; i--) {
+		cout << i << "\n";
+	}
+	
+	return 0;
+}
