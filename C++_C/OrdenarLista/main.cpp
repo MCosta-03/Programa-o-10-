39904cd0c77adc2012fa613f3420aadb345568e5
MCosta-03 <gilcostabn@gmail.com>
2025-10-27 12:41:05 +0000

    Update main.cpp

--- C++_C/OrdenarLista/main.cpp
+++ C++_C/OrdenarLista/main.cpp
@@ -9,7 +9,6 @@ int main() {
 	setlocale(LC_ALL, "Portuguese");
 
 	int list[10] = { 15, 2, 53, 4, 1, 17, 5, 23, 45, 19 };
-	int Out_list[10];
 	int AUX = 0;
 	int trocas = 0;
 
@@ -28,4 +27,5 @@ int main() {
 	for (int i = 0; i < 10; i++)
 		cout << list[i] << "\n";
 	return 0;
+
 }
