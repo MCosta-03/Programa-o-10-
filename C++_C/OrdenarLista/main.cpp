22b344a71c22ae362ff72a1be886d662e305cf5a
MCosta-03 <gilcostabn@gmail.com>
2025-10-27 12:32:18 +0000

    Add files via upload

---
+++ C++_C/OrdenarLista/main.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <locale>
+#include <string>
+#include <stdlib.h>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Portuguese");
+
+	int list[10] = { 15, 2, 53, 4, 1, 17, 5, 23, 45, 19 };
+	int Out_list[10];
+	int AUX = 0;
+	int trocas = 0;
+
+	do {
+		trocas = 0;
+		for (int i = 0; i < 10; i++) {
+			if (list[i] > list[i + 1]) {
+				AUX = list[i];
+				list[i] = list[i + 1];
+				list[i+1] =AUX;
+				trocas++;
+			}
+		}
+	} while (trocas != 0);
+
+	for (int i = 0; i < 10; i++)
+		cout << list[i] << "\n";
+	return 0;
+}
