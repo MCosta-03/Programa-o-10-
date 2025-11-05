aa757214519fe169a000a6ac8db6e1a2f28108a3
MCosta-03 <gilcostabn@gmail.com>
2025-11-05 13:22:30 +0000

    Add files via upload

---
+++ C++_C/LinguaP/main.cpp
@@ -0,0 +1,34 @@
+﻿/*
+*   A lígua dos P´S é estranha, Coisa marada..
+*/
+
+#include <iostream>
+#include <locale>
+#include <string>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+
+    string word;
+    string result;
+
+    cout << "Diz uma palavra: ";
+    cin >> word;
+
+    //strlen -> obter o tamanho da palavra
+    for (int i = 0; i < strlen(word.c_str()); i++) {
+        result += word[i];
+        //Verificar se é uma vogal, em caso de minúscula ou maiúscula
+        if (word[i] == 'a' || word[i] == 'e' || word[i] == 'i' || word[i] == 'o' || word[i] == 'u' ||
+            word[i] == 'A' || word[i] == 'E' || word[i] == 'I' || word[i] == 'O' || word[i] == 'U') {
+            result += 'p';      
+            result += word[i];
+        }
+    }
+    cout << result;
+    return 0;
+}
