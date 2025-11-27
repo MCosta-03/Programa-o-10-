72db21bc4365c1d1e92dca4260c4c1d482667094
MCosta-03 <gilcostabn@gmail.com>
2025-11-27 15:50:36 +0000

    Add files via upload

--- C++_C/NotasETurmas/main.cpp
+++ C++_C/NotasETurmas/main.cpp
@@ -9,30 +9,49 @@ using namespace std;
 int main() {
     setlocale(LC_ALL, "Portuguese");
 
-    int turma[3][3];
+    string nomes[10] = {"António", "Anabela", "Beatriz", "Bernardo", "Clara", "Carlos", "Diana", "Diogo", "Elisabete", "Eurico"};
+    string disciplinas[10] = {"Portugues", "Ingles", "Fisica", "TLP", "TIC", "Matemática", "ACS", "Filosofia", "Quimica", "Ed.Fis."};
+
+    int turma[10][10];
     int total = 0;
 
-    for (int y = 0; y < 3; y++) {
-        for (int x = 0; x < 3; x++) {
-            cout << "Diz a nota do " << x+1 << "º aluno da turma " << y+1 << ": ";
+    int A_nota_especial = 0;
+    string disciplina_do_especial;
+    string nome_do_especial;
+
+    for (int y = 0; y < 10; y++) {
+        for (int x = 0; x < 10; x++) {
+            cout << "Diz a nota do " << nomes[x] << " na disciplina " << disciplinas[y] << ": ";
             cin >> turma[x][y];
+
+            if (A_nota_especial < turma[x][y]) {
+                A_nota_especial = turma[x][y];
+                disciplina_do_especial = disciplinas[y];
+                nome_do_especial = nomes[x];
+            }
         }
     }
 
-    for (int y = 0; y < 3; y++) {
-        for (int x = 0; x < 3; x++) {
+    for (int y = 0; y < 10; y++) {
+        for (int x = 0; x < 10; x++) {
             total += turma[x][y];
         }
-        cout << "A média da nota da turma " << y + 1 << " foi: " << total / 3 << endl;
+        cout << "A média da nota da disciplina de " << disciplinas[y] << " foi: " << total / 10 << endl;
         total = 0;
     }
 
-    for (int y = 0; y < 3; y++) {
-        for (int x = 0; x < 3; x++) {
+    for (int y = 0; y < 10; y++) {
+        for (int x = 0; x < 10; x++) {
             total += turma[x][y];
         }
     }
-    cout << "\nA média de notas das turmas foi: " << total / 9 << endl;
 
+    cout << "\nA média de notas das disciplinas foi: " << total / (10*10) << endl;
+    cout << "E a maior nota foi: " << A_nota_especial << " de " << nome_do_especial << " na disciplina de " << disciplina_do_especial << endl;
+
+    cout << "Enter para sair...";
+
+    cin.ignore();
+    cin.get();
     return 0;
 }
