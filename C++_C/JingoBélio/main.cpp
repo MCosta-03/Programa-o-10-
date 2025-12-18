b6a1ac358cdcdc91f1ef5de8b2883f24422d2b94
MCosta-03 <gilcostabn@gmail.com>
2025-12-18 16:25:22 +0000

    Update main.cpp

--- C++_C/JingoBélio/main.cpp
+++ C++_C/JingoBélio/main.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <locale>
 #include <string>
 #include <stdlib.h>
@@ -11,7 +11,7 @@ int main() {
     string nomes[3] = { "Mateus", "Clara", "Brunão" };
     string produtos[3] = { "Batatas Fritas", "Bolo rei", "Perfume" };
 
-    int quantidades[3][3], total = 0;
+    int quantidades[3][3], total = 0, total_por_pessoa[3] = {0 };
 
     for (int i = 0; i < 3; i++)
     {
@@ -20,11 +20,16 @@ int main() {
             cout << "Diz a quantidade de " << produtos[j] << " para " << nomes[i] << ": ";
             cin >> quantidades[i][j];
             total += quantidades[i][j];
+            total_por_pessoa[i] += quantidades[i][j];
         }
     }
     cout << "O total de produtos foi : " << total << endl;
     cout << "O total de " << produtos[0] << " foi de " << quantidades[0][0] + quantidades[1][0] + quantidades[2][0] << endl;
     cout << "O total de " << produtos[1] << " foi de " << quantidades[0][1] + quantidades[1][1] + quantidades[2][1] << endl;
     cout << "O total de " << produtos[2] << " foi de " << quantidades[0][2] + quantidades[1][2] + quantidades[2][2] << endl;
+
+    cout << "O total do " << nomes[0] << "foi: " << total_por_pessoa[0] << endl;
+    cout << "O total do " << nomes[1] << "foi: " << total_por_pessoa[1] << endl;
+    cout << "O total do " << nomes[2] << "foi: " << total_por_pessoa[2] << endl;
     return 0;
 }
